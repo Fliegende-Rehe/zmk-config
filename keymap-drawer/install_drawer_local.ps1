24850318e3d0a97aeafc3db45029bd4bafe8ae44
Fliegende-Rehe <fliegende-rehe@yandex.ru>
2025-12-30 17:02:14 +0300

    scoliosis setup

--- keymap-drawer/install_drawer_local.ps1
+++
@@ -1,8 +0,0 @@
-# py -m pip uninstall --user pipx
-py -m pip install --user pipx
-pipx ensurepath
-# pipx uninstall keymap-drawer
-pipx install keymap-drawer
-& "$env:USERPROFILE\pipx\venvs\keymap-drawer\Scripts\python.exe" -m pip uninstall tree-sitter -y
-& "$env:USERPROFILE\pipx\venvs\keymap-drawer\Scripts\python.exe" -m pip install tree-sitter==0.24.0
-& "$env:USERPROFILE\pipx\venvs\keymap-drawer\Scripts\python.exe" -m pip show tree-sitter
